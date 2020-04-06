86aeaf2b3016fffd1166f8056469a9ff00b7c52a
tmbern <tmbern42@gmail.com>
2020-04-06 15:06:36 -0600

    update script file

--- my_lambdata_tmbern_Unit3/my_script.py
+++ my_lambdata_tmbern_Unit3/my_script.py
@@ -1,9 +1,9 @@
-import pandas as pd
-import numpy
+# import pandas as pd
+# import numpy
 
-df = pd.DataFrame({'x': [1, 2, 3, 4, 5,np.nan],
- 'y': [np.nan,np.nan , 1, 2, 3, 4 ],
- 'z': [1, 2, 3, 4, 5, 6]})
+# df = pd.DataFrame({'x': [1, 2, 3, 4, 5,np.nan],
+#  'y': [np.nan,np.nan , 1, 2, 3, 4 ],
+#  'z': [1, 2, 3, 4, 5, 6]})
 
 
-from lambdata_tmbern_DS13.my_mod import check_for_nulls
+# from lambdata_tmbern_DS13.my_mod import check_for_nulls
