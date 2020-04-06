3bfebfd7a77eb386944f13f171b0d3410296faa3
tmbern <tmbern42@gmail.com>
2020-04-06 14:33:08 -0600

    update the split dates function to include converting column to datetime format

--- my_lambdata_tmbern_Unit3/my_mod.py
+++ my_lambdata_tmbern_Unit3/my_mod.py
@@ -1,5 +1,7 @@
 # Check a dataframe for nulls, print/report them in a nice "pretty" format:
 
+import pandas
+
 def check_for_nulls(df):
     
     print(df.isnull().sum())
@@ -8,8 +10,13 @@ def check_for_nulls(df):
 
 def split_dates(df, column):
     
+    # create a copy to prevent copy warnings
     df = df.copy()
 
+    # convert the column to a date time format
+    df[column] = pandas.to_datetime(df[column], infer_datetime_format=True)
+
+    # create new columns for the month day and year of the given date column
     df['Month'] = df[column].dt.month
     df['Day'] = df[column].dt.day
     df['Year'] = df[column].dt.year
